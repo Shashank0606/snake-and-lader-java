987b56c687c0f6873eddd0cddfe32c2a7388488b
Shashank0606 <shashankrathore606@gmail.com>
2022-11-22 07:48:46 +0530

    ADD: Starting Position

---
+++ snakeandladder/src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package SnakeAndLadder.snakeandladder.src.main.java.com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int pos = 0;
+        System.out.println("Welcome to Snake And Ladder game");
+        System.out.println("player start position is " + pos);
+
+    }
+
+}
